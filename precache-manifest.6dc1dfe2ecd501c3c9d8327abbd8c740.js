0097bc6686b2dbc615bdfa2565859b76a09ee8a2
Nick Lin <nicklin@Nicks-MacBook-Pro.local>
2018-12-14 23:13:26 -0800

    Updates

---
+++ precache-manifest.6dc1dfe2ecd501c3c9d8327abbd8c740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "6f99bad58ec44eacc3de",
+    "url": "/website/static/js/main.6f99bad5.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/website/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "6f99bad58ec44eacc3de",
+    "url": "/website/static/css/main.9141f313.chunk.css"
+  },
+  {
+    "revision": "0aa6f19ac81ee04e1a0cb96b6a900b10",
+    "url": "/website/index.html"
+  }
+];
