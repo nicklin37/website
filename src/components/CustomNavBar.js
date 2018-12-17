f617a05388af975e3668d5d9c3a35e53d0bca096
Nick Lin <nicklin369@gmail.com>
2018-12-16 21:08:35 -0800

    navbar styling

--- src/components/CustomNavBar.js
+++ src/components/CustomNavBar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../styles/CustomNavBar.css';
 
 export default class CustomNavBar extends Component{
     render(){
