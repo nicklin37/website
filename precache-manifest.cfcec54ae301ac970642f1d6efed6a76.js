60753da63dccfcbbd9bf4f646e7bae69edc0245d
nicklin37 <nicklin369@gmail.com>
2019-01-06 22:45:09 -0800

    Updates

---
+++ precache-manifest.cfcec54ae301ac970642f1d6efed6a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c7ce46fcab78362c7ed2e99d144d58",
+    "url": "/website/static/media/edit_5.15c7ce46.jpg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "2fad0b76544876219631",
+    "url": "/website/static/js/main.2fad0b76.chunk.js"
+  },
+  {
+    "revision": "7a262955f5383120e9e7",
+    "url": "/website/static/js/1.7a262955.chunk.js"
+  },
+  {
+    "revision": "2fad0b76544876219631",
+    "url": "/website/static/css/main.30482001.chunk.css"
+  },
+  {
+    "revision": "e6927f6af1eb083205b9a6c182f62e5b",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.4330daec349750f6a3c4dfc4bbcbe967.js"
+  "/website/precache-manifest.cfcec54ae301ac970642f1d6efed6a76.js"
 );
 
 workbox.clientsClaim();
